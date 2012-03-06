cf6143eb9932d9c7984b5619a5f83347cc97eaf0
Reinardus Pradhitya <s.pradhitya@gmail.com>
2012-03-06 19:19:47 +0800

    moving square with key-checking inside loop

--- tetris.py
+++ tetris.py
@@ -21,14 +21,23 @@ HALF_WIDTH=20
 FULL_WIDTH=2*HALF_WIDTH
 LINE_WIDTH=3
 
+#pygame things
+clock=pygame.time.Clock()
 screen=pygame.display.set_mode((WIDTH,HEIGHT));
-#surface set_mode(height,width,flag,color). Color will be automatically fitted if not supplied
 
 class Square:
     def __init__(self,color,pos):
         self.color=color
         self.x=pos[0]
         self.y=pos[1]
+    def move_up(self):
+        self.y=self.y-FULL_WIDTH
+    def move_down(self):
+        self.y=self.y+FULL_WIDTH
+    def move_left(self):
+        self.x=self.x-FULL_WIDTH
+    def move_right(self):
+        self.x=self.x+FULL_WIDTH
     def draw(self):
         corners=[]
         corners.append((self.x-HALF_WIDTH,self.y-HALF_WIDTH))
@@ -44,20 +53,31 @@ class Tetris():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Adhit's Tetris")
-        self.test_square=Square(RED,(40,40))
+        self.test_square=Square(RED,(20,20))
+    def handle_key_event(self,square):
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            square.move_up()
+        if keys[pygame.K_DOWN]:
+            square.move_down()
+        if keys[pygame.K_LEFT]:
+            square.move_left()
+        if keys[pygame.K_RIGHT]:
+            square.move_right()
     def draw(self,square):
         screen.fill(BG_COLOR)
         square.draw()
         pygame.display.flip()
     def main_loop(self):
         while True: #game loop: read_events->update_data->draw_objects
+            clock.tick(10) #iterations in one second
             #read_events
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     sys.exit()
-            #update_data:not yet
+            self.handle_key_event(self.test_square)
             #draw_objects
-            self.draw(self.test_square)            
+            self.draw(self.test_square)       
 
 if __name__=='__main__':
     tetris=Tetris()
